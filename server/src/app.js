4df74501e57294a8a5202620ba153f0923bcb045
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-03-24 18:30:43 +0100

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+	logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+	return { hello: 'world' };
+});
+
+try {
+	await fastify.listen({ port: 3000, host: '0.0.0.0' });
+} catch (err) {
+	fastify.log.error(err);
+	process.exit(1);
+}
