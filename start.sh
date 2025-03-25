a55a6078c37009365637559030621711ede5c5d3
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-03-25 22:09:18 +0100

    chores

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Navigate to client directory and start TypeScript watch in background
 echo "Starting TypeScript watch..."
 cd client
-npm run build & # Run tsc --watch in the background
+npm run build &
 cd ..
 
-# Start Docker Compose
 echo "Starting Docker containers..."
 docker-compose up --build
