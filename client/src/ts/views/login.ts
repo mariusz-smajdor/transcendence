3f47ef021917accee72a6e5fb59200fa09fcdec5
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-01 22:32:44 +0200

    add login component

--- client/src/ts/views/login.ts
+++ client/src/ts/views/login.ts
@@ -1,10 +1,75 @@
-export default function Login(): HTMLElement {
-	const section = document.createElement('section');
-	
+import { Button, GoogleButton } from '../components/button.js';
+
+function Separator(): HTMLElement {
+	const wrapper = document.createElement('div');
+	wrapper.classList.add(
+		'flex',
+		'items-center',
+		'justify-center',
+		'mb-4',
+		'text-gray-500'
+	);
+
+	const separator = document.createElement('div');
+	separator.classList.add(
+		'border-b-1',
+		'border-gray-300',
+		'h-1',
+		'w-full',
+		'mx-1'
+	);
+
+	const span = document.createElement('span');
+	span.textContent = 'or';
+
+	wrapper.appendChild(separator);
+	wrapper.appendChild(span);
+	wrapper.appendChild(separator.cloneNode(true));
+
+	return wrapper;
+}
+
+function Input(type: string, placeholder: string): HTMLElement {
+	const input = document.createElement('input');
+
+	input.type = type;
+	input.classList.add(
+		'rounded',
+		'border',
+		'border-gray-300',
+		'p-2',
+		'mb-4',
+		'w-full'
+	);
+	input.placeholder = placeholder;
+
+	return input;
+}
+
+function Heading(): HTMLElement {
 	const heading = document.createElement('h1');
 	heading.textContent = 'Login';
-	
-	section.appendChild(heading);
-	
-	return section;
+	heading.classList.add('text-2xl', 'font-bold', 'mb-4');
+
+	return heading;
+}
+
+export default function Login(): HTMLElement {
+	// create secrion container element
+	const form = document.createElement('form');
+	form.classList.add('max-w-md', 'mx-auto', 'mt-10', 'p-6');
+
+	form.appendChild(Heading());
+	form.appendChild(Input('text', 'Username'));
+	form.appendChild(Input('password', 'Password'));
+	form.appendChild(
+		Button({
+			type: 'submit',
+			content: 'Sign Up',
+		})
+	);
+	form.appendChild(Separator());
+	form.appendChild(GoogleButton());
+
+	return form;
 }
