1b0555860b373cacbdf530dc1140799b4ce7671a
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-02 20:58:01 +0200

    save registered user to sqlite database

--- server/src/app.js
+++ server/src/app.js
@@ -1,33 +1,59 @@
 import Fastify from 'fastify';
 import multipart from '@fastify/multipart';
 import cors from '@fastify/cors';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'path';
 
-const fastify = Fastify({
-	logger: true,
-});
+const fastify = Fastify({ logger: true });
 
 fastify.register(multipart);
 fastify.register(cors, {
 	origin: '*',
 	methods: ['GET', 'POST'],
 	allowedHeaders: ['Content-Type'],
-})
+});
+
+const db = await open({
+	filename: path.join(process.cwd(), 'database', 'database.sqlite'),
+	driver: sqlite3.Database,
+});
+
+await db.exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT UNIQUE NOT NULL,
+		email TEXT UNIQUE NOT NULL,
+		password TEXT NOT NULL
+	);
+`);
 
 fastify.get('/', async (request, reply) => {
-	return { hello: 'world' };
+	try {
+    const users = await db.all('SELECT * FROM users');
+    reply.send({ success: true, users });
+  } catch (error) {
+    reply.status(500).send({ success: false, error: error.message });
+  }
 });
 
 fastify.post('/register', async (request, reply) => {
-  const parts = request.parts();
-  const formData = {};
+	try {
+		const parts = request.parts();
+		const formData = {};
 
-  for await (const part of parts) {
-      formData[part.fieldname] = part.value;
-  }
+		for await (const part of parts) {
+			formData[part.fieldname] = part.value;
+		}
+		const result = await db.run(
+			'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+			[formData.username, formData.email, formData.password]
+		);
 
-  console.log('Received Data:', formData);
-  
-  reply.send({ success: true, data: formData});
+		reply.send({ success: true, userId: result.lastID });
+	} catch (error) {
+		reply.status(500).send({ success: false, error: error.message });
+	}
 });
 
 try {
