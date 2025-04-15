2fb3131664b1c386a9a358d7c45f8cbb1afaadfb
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-15 22:57:24 +0200

    add horizontal padding to container component

--- client/src/ts/components/container.ts
+++ client/src/ts/components/container.ts
@@ -1,6 +1,6 @@
 export function Container(): HTMLDivElement {
 	const container: HTMLDivElement = document.createElement('div');
-	container.classList.add('container', 'mx-auto');
+	container.classList.add('container', 'mx-auto', 'px-6');
 
 	return container;
 }
