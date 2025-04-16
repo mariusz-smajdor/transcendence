47376f30f7f3020d590f3193e00a5bd19e22523b
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-16 23:48:17 +0200

    stop event propagation

--- client/src/ts/components/icon.ts
+++ client/src/ts/components/icon.ts
@@ -7,5 +7,7 @@ export function Icon({ name, classes = [] }: IconProps) {
 	const icon = document.createElement('i');
 	icon.classList.add(...name, ...classes!);
 
+	icon.addEventListener('click', (event) => event.stopPropagation());
+
 	return icon;
 }
