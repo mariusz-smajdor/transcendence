c1b3074e0fa51e08a84166cf00b896f85a3927ca
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-16 23:48:43 +0200

    add dynamic classes to container element

--- client/src/ts/components/container.ts
+++ client/src/ts/components/container.ts
@@ -1,6 +1,6 @@
-export function Container(): HTMLDivElement {
+export function Container(classes: string[] = []): HTMLDivElement {
 	const container: HTMLDivElement = document.createElement('div');
-	container.classList.add('container', 'mx-auto', 'px-6');
+	container.classList.add('container', 'mx-auto', 'px-6', ...classes);
 
 	return container;
 }
