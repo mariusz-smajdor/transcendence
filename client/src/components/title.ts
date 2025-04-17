2e5ed2152707df397ba17c9726ffd68333777818
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-17 22:30:45 +0200

    refactor title component

--- client/src/components/title.ts
+++ client/src/components/title.ts
@@ -1,13 +1,14 @@
-interface TitileProps {
+import { ComponentProps } from '../types/component';
+
+interface TitileProps extends ComponentProps {
 	level: 1 | 2 | 3 | 4 | 5 | 6;
 	content: string;
-	classes?: string[];
 }
 
 export function Title({ level, content, classes = [] }: TitileProps) {
-	const title = document.createElement(`h${level}`) as HTMLHeadingElement;
-	title.textContent = content;
-	title.classList.add('text-xl', ...classes!);
+	const h = document.createElement(`h${level}`);
+	h.textContent = content;
+	h.classList.add('text-xl', ...classes!);
 
-	return title;
+	return h;
 }
