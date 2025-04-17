a3c9374331d73561cd3fb2f4b49625f2c571432e
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-17 22:32:06 +0200

    refactor header layout component
    created only logo for now

--- client/src/layout/header.ts
+++ client/src/layout/header.ts
@@ -1,12 +1,47 @@
-import { createElement, ArrowRight } from 'lucide';
+import { CircleMinus } from 'lucide';
+
+import { Container } from '../components/container';
+import { Wrapper } from '../components/wrapper';
+import { Icon } from '../components/icon';
+import { Title } from '../components/title';
+
+function Logo() {
+	const wrapper = Wrapper({
+		classes: ['flex', 'items-center', 'gap-2', 'cursor-pointer', 'select-none'],
+	});
+	wrapper.addEventListener('click', () => {
+		window.location.href = '/';
+	});
+
+	const title = Title({
+		level: 2,
+		content: 'Super Pong',
+		classes: ['text-nowrap'],
+	});
+	const icon = Icon({ icon: CircleMinus });
+
+	wrapper.appendChild(icon);
+	wrapper.appendChild(title);
+
+	return wrapper;
+}
 
 export default function Header() {
 	const header = document.createElement('header');
-	header.classList.add('flex', 'h-14', 'w-full', 'items-center', 'shadow-sm');
+	header.classList.add(
+		'flex',
+		'items-center',
+		'h-14',
+		'w-full',
+		'border-b',
+		'border-border'
+	);
 
-	const lucideIcon = createElement(ArrowRight);
-	header.appendChild(lucideIcon);
+	const container = Container({
+		classes: ['flex', 'items-center'],
+	});
 
-	// header.appendChild(container);
+	container.appendChild(Logo());
+	header.appendChild(container);
 	return header;
 }
