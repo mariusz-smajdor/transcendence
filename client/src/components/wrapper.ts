2ae1471f00395ecbe39313ec6a97e4c1def8346d
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-19 23:54:12 +0200

    type refactor

--- client/src/components/wrapper.ts
+++ client/src/components/wrapper.ts
@@ -1,6 +1,6 @@
 import { type ComponentProps } from '../types/component';
 
-export function Wrapper({ classes }: ComponentProps) {
+export function Wrapper({ classes = [] }: ComponentProps) {
 	const div = document.createElement('div');
 	div.classList.add(...classes!);
 
