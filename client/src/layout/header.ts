3044e13f9fb2d5b8ff3b4a92d79be25aa0a8b5de
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-21 01:20:19 +0200

    add blurry transparent styles to header

--- client/src/layout/header.ts
+++ client/src/layout/header.ts
@@ -119,12 +119,17 @@ function Logo() {
 export default function Header() {
 	const header = document.createElement('header');
 	header.classList.add(
+		'fixed',
+		'top-0',
+		'z-50',
 		'flex',
 		'h-14',
 		'w-full',
 		'items-center',
 		'border-b',
-		'border-accent'
+		'border-accent',
+		'backdrop-blur-sm',
+		'bg-background/50'
 	);
 
 	const container = Container({
