fc0e08cb32eab8eed9db03375471ea279fafb6cc
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-30 18:07:23 +0200

    basic setup

---
+++ client/src/router.ts
@@ -0,0 +1,40 @@
+import Home from './views/home';
+
+export class Router {
+	private rootElement: HTMLElement | null = document.getElementById('app');
+	private routes: { [key: string]: () => Node };
+
+	constructor() {
+		this.routes = {
+			'/': Home,
+		};
+
+		window.addEventListener('popstate', this.loadRoute.bind(this));
+		document.body.addEventListener('click', (event) => {
+			const target = event.target as HTMLAnchorElement;
+			if (target.matches('[data-link]')) {
+				event.preventDefault();
+				this.navigateTo(target.href);
+			}
+		});
+
+		this.loadRoute();
+	}
+
+	navigateTo(url: string) {
+		console.log(url);
+		alert('Navigating to ' + url);
+		history.pushState(null, '', url);
+		this.loadRoute();
+	}
+
+	private loadRoute() {
+		const path = location.pathname;
+
+		const view = this.routes[path];
+		if (this.rootElement) {
+			this.rootElement.innerHTML = '';
+			this.rootElement.appendChild(view());
+		}
+	}
+}
