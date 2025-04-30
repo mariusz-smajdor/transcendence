dcf82e57157e969a03147772eaad44831a3a8ffe
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-04-30 23:18:41 +0200

    add reusable components

---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+export { Wrapper } from './wrapper';
+export { Container } from './container';
+export { Heading } from './heading';
+export { Button } from './button';
+export { Icon } from './icon';
+export { Span } from './span';
