df46317364719b49a09b474c236b12caa3b27c91
Mariusz Smajdor <smajdormariusz98@gmail.com>
2025-05-03 22:40:22 +0200

    register user to backend

--- client/src/components/wrapper.ts
+++ client/src/components/wrapper.ts
@@ -1,12 +1,20 @@
 import { type ComponentProps } from '../types/component';
 
-type WrapperProps = ComponentProps & {
-	element?: 'div' | 'main' | 'section' | 'header' | 'form';
-};
+type WrapperProps =
+	| (ComponentProps & { element?: 'div' | 'main' | 'section' | 'header' })
+	| (ComponentProps & { element: 'form'; method: 'GET' | 'POST' });
 
-export function Wrapper({ element = 'div', classes = [] }: WrapperProps) {
+export function Wrapper(props: WrapperProps) {
+	const { element = 'div', classes = [] } = props;
+
 	const wrapper = document.createElement(element);
-	wrapper.classList.add(...classes!);
+	wrapper.classList.add(...classes);
+
+	// Type narrowing to access `method` safely
+	if (element === 'form') {
+		const formMethod = (props as { method: 'GET' | 'POST' }).method;
+		wrapper.setAttribute('method', formMethod);
+	}
 
 	return wrapper;
 }
