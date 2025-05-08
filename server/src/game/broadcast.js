5b5e7471deec8c80ef6e5d144e1b607ef44f94b1
fjalowiecki <f.jalowiecki@gmail.com>
2025-05-08 09:41:17 +0200

    fix connection issues for players and spectators

--- server/src/game/broadcast.js
+++ server/src/game/broadcast.js
@@ -6,7 +6,20 @@ export function broadcastGameState(clients, gameState) {
                 data: gameState
               }));
         } catch (err) {
-          console.error('Błąd przy wysyłaniu:', err);
+          console.error('Error when sending:', err);
+        }
+    }
+}
+
+export function broadcastMessage(clients, message) {
+    for (const client of clients) {
+        try {
+            client.send(JSON.stringify({
+                type: 'message',
+                message: message
+            }));
+        } catch (err) {
+            console.error('Error when sending:, err');
         }
     }
 }
--- server/src/routes/game.js
+++ server/src/routes/game.js
@@ -1,7 +1,5 @@
 import { registerGameWebSocket } from "../game/gameWs.js";
-import { playersManager } from "../game/players.js";
-import { gameState, updateGameState , getGameStateProportional} from "../game/gameState.js";
-import { broadcastGameState } from "../game/broadcast.js";
+import { gameState } from "../game/gameState.js";
 
 const clients = new Set();
 
