0eb11e38963e53b3e345e4f9d1f30712a83edbed
Grabarz01 <filipgrabowski2@gmail.com>
2025-05-19 13:47:30 +0200

    refactor keybord setup

--- client/src/views/game/game-keys.ts
+++ client/src/views/game/game-keys.ts
@@ -1,5 +1,3 @@
-import { GameState } from '../../types/game';
-
 function handleReadyKey(ws: WebSocket, e: KeyboardEvent) {
     if (e.key === 'r' || e.key === 'R') {
         ws.send('READY');
@@ -13,17 +11,12 @@ function handleReadyKey(ws: WebSocket, e: KeyboardEvent) {
 //     }
 // }
 
-export function setupKeyboardControls(ws: WebSocket, gameState: GameState) {
-    document.addEventListener('keydown', (e: KeyboardEvent) => {
-        if (gameState.playerRole === 'left') {
-            if (e.key === 'w' || e.key === 'ArrowUp') ws.send('UP');
-            if (e.key === 's' || e.key === 'ArrowDown') ws.send('DOWN');
-        } else if (gameState.playerRole === 'right') {
-            if (e.key === 'w' || e.key === 'ArrowUp') ws.send('UP');
-            if (e.key === 'w' || e.key === 'ArrowDown') ws.send('DOWN');
-        }
-        handleReadyKey(ws, e);
-    });
+export function setupKeyboardControls(ws: WebSocket) {
+	document.addEventListener('keydown', (e: KeyboardEvent) => {
+		if (e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') ws.send('UP');
+		else if (e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') ws.send('DOWN');
+		handleReadyKey(ws, e);
+	});
 }
 
 export function setupKeyboardControlsForLocal(ws: WebSocket) {
@@ -32,14 +25,6 @@ export function setupKeyboardControlsForLocal(ws: WebSocket) {
         else if (e.key === 's' || e.key === 'S') ws.send('LEFT_DOWN');
         else if (e.key === 'ArrowUp') ws.send('RIGHT_UP');
         else if (e.key === 'ArrowDown') ws.send('RIGHT_DOWN');
-        handleReadyKey(ws, e);
-    });
-}
-
-export function setupKeyboardControlsForAI(ws: WebSocket) {
-    document.addEventListener('keydown', (e: KeyboardEvent) => {
-        if (e.key === 'w' || e.key === 'W' || e.key === 'ArrowUp') ws.send('UP');
-        else if (e.key === 's' || e.key === 'S' || e.key === 'ArrowDown') ws.send('DOWN');
         handleReadyKey(ws, e);
     });
 }
