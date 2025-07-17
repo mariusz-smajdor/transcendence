a474533b3e8d9ec58ead8615a37918355d75730d
Grabarz01 <filipgrabowski2@gmail.com>
2025-07-17 19:13:31 +0200

    further development of data structures and api for tournament

--- server/src/routes/tournament.js
+++ server/src/routes/tournament.js
@@ -5,12 +5,13 @@ import { clients, notAuthenticated } from "./game";
 const tournaments = new Tournaments();
 
 export async function tournamentRoute(fastify){
-
+	//check for available tournamnets or open the current tournament
 	fastify.post('/tournament/rooms', async (req, res) =>{
 		let {token , sessionId} = req.body;
 		let userRoom = tournaments.userTournament(sessionId,token);
+		let rooms;
 		if (userRoom){
-			const rooms = {
+			rooms = {
 				found: true,
 				id: userRoom.roomId,
 				creator: userRoom.creator,
@@ -18,7 +19,7 @@ export async function tournamentRoute(fastify){
 				playersExpected: 8};
 		}
 		else{
-			const rooms = Array.from(tournaments.rooms).map(room => ({
+			rooms = Array.from(tournaments.rooms).map(room => ({
 				found: false,
 				id: room.roomId,
 				creator: room.creator,
@@ -26,49 +27,92 @@ export async function tournamentRoute(fastify){
 				playersExpected: 8
 			}));
 		}
-		return {rooms};
+		res.code(200).send(rooms);
 	})
 
-	//in progres....
+	//creat tournament room
 	fastify.post('/tournament/create'), async (req, res) =>{
-		const { creator } = req.body;
-		
+		const { creator,token,sessionId,numberOfPlayers} = req.body;
+		const connection = getWs(sessionId,token,res);
+
+		let roomId = tournaments.createRoom(connection,creator,numberOfPlayers,token,sessionId);
+		res.code(200).send({
+			id: roomId,
+			creator: creator,
+			playersIn: 1,
+			playersExpected: numberOfPlayers
+		});
 	}
 
 	fastify.post('/tournament/join'), async (req, res) =>{
 		const { name,token,sessionId,roomId } = req.body;
-		tournaments.joinRoom()
+		let connection = getWs(sessionId, token,res);
 
-		
-		
+		let room = tournaments.joinRoom(roomId,connection,name,token,sessionId);
+
+		if (room.players.size == room.playersExpected.size){
+			res.code(200).send({
+					id: roomId,
+					creator: room.creator,
+					playersIn: room.players.size,
+					playersExpected: room.playersExpected.size,
+					bracket: room.getDraw()
+			});
+		}
+		else{
+			res.code(200).send({
+					id: roomId,
+					creator: room.creator,
+					playersIn: room.players.size,
+					playersExpected: room.playersExpected.size
+			});
+		}
 	}
 
-	fastify.get('/tournament', { websocket: true }, (connection, req) => {
-    const { gameId } = req.query;
-    const game = games.get()
-    console.log('New WebSocket connection - address:', req.socket.remoteAddress);
-
-    if (!game) {
-      connection.send(JSON.stringify({ error: 'Game not found' }));
-      connection.close();
-      console.log('Game not found');
-      return;
-    }
-    manageLocalGameWebSocket(game, connection, games, gameId);
+	fastify.get('/tournament/start', async (req, res) => {
+		const { roomId } = req.query;
+		let room = tournaments.getRoom(roomId);
+		if (room === undefined)
+			res.code(400).send({error: "Error: Cannot found tournament with this id"});
+		if (room.playersExpected.size != room.playersIn.length)
+			res.code(400).send({error: "Error: Wrong amount of players"});
+		room.createMatches();
+		res.code(200).send({success: true});
   });
 
-	fastify.get('/tournamentgame', { websocket: true }, (connection, req) => {
-    const { roomId, gameId } = req.query;
-    const game = games.get()
-    console.log('New WebSocket connection - address:', req.socket.remoteAddress);
-
-    if (!game) {
-      connection.send(JSON.stringify({ error: 'Game not found' }));
+	fastify.post('/tournament/play', { websocket: true }, (connection, req) => {
+    const {roomId , matchNumber} = req.query;
+		
+		const room = tournaments.getRoom(roomId);
+		if (room === undefined)
+		{
+			connection.send(JSON.stringify({ error: 'Room not found' }));
       connection.close();
-      console.log('Game not found');
+      console.log('Room not found');
       return;
-    }
-    manageLocalGameWebSocket(game, connection, games, gameId);
+		}
+		const match = room.match.get(parseInt(matchNumber));
+		if (match === undefined)
+		{
+			connection.send(JSON.stringify({ error: 'Match not found' }));
+      connection.close();
+      console.log('Match not found');
+      return;
+		}
+    manageMatchWebSocket(tournaments,match,connection);
   });
 
+}
+
+function getWs(sessionId, token, res){
+	if (!token && !sessionId){
+		res.code(400).send({error: "Missing token or sessionId. Cannot identify user."});
+		return null;
+	}
+	const connection = token ? clients.get(token) : notAuthenticated.get(sessionId)
+	if (connection === undefined){
+		res.code(400).send({error: "Wrong token or sessionId. Cannot identify user."});
+		return null;
+	}
+	return connection;
 }
--- server/src/tournament/tournaments.js
+++ server/src/tournament/tournaments.js
@@ -1,28 +1,35 @@
 import { uuidv4 } from "uuid";
 import { initGame } from "../game/gameState";
+import { clients, notAuthenticated } from "../routes/game";
 
 export class Tournaments{
 	rooms = new Map();// roomId : room
 	
+	getRoom(roomId){
+		return this.rooms.get(roomId);
+	}
+
 	joinRoom(roomId,connection,nickname,token = null, sessionId = null){
 		let room = this.rooms.get(roomId);
 		room.addPlayer(connection,nickname,token,sessionId);
+		return room;
 	}
 
 	createRoom(connection, creator, numberOfPlayers, token = null, session = null){
-		const room  = new Room(creator);
+		const room  = new Room(creator, numberOfPlayers);
 		const roomId = uuidv4()
 		room.addPlayer(connection,creator,token,sessionId);
 		rooms.set(roomId,room);
-
+		return roomId;
 	}
 
 	userTournament(sessionId = null, token = null){
+
 		for (const room of this.rooms){
 			for (const player of room.players){
 				if (token && player.token === token)
 					return room;
-				if (!token && player.connection === connection)
+				if (!token && player.sessionId === sessionId)
 					return room;
 			}
 		}
@@ -33,18 +40,27 @@ export class Tournaments{
 
 export class Room{
 	tournamentGames = new Map(); // matchID: Match
-	players = new Array(); //tmpId: Player
+	players = new Array(); //Player
 	expectedPlayers = new Set();
 	courentRound = 1;
-	matches = new Set();
+	matches = new Map(); //matchNumber: Match
 	matchesToPlay = 0;
-	matchesPlayed = 0;
+	matchesCreated = 0;
+	matchesPlayed = 0
 	creator = null;
 
 	constructor(creator,numberOfPlayers){
 		this.creator = creator;
 		setExpectedPlayers(numberOfPlayers);
 		setMatchesToPlay();
+	}
+
+	//to do:checking if the user joined the match, if not walkover
+	sendNofification(connection, matchNum){
+		connection.send(JSON.stringify({
+			type: 'join',
+			matchNumber: match.matchNumber
+		}));
 	}
 
 	setExpectedPlayers(numberOfPlayers){
@@ -66,10 +82,34 @@ export class Room{
 		}
 	}
 
+	getDraw(){
+		this.tournamentDraw();
+		let draw = [];
+		this.players.forEach(element => {
+			draw.push({id: element.tmpId, nickname: element.nickname});
+		});
+		return draw;
+	}
+
 	addPlayer(connection,nickname,token,sessionId){
 		this.players.add(new Player(connection,nickname,token,nickname));
 	}
 
+	getPlayersForMatch(match, matchNumber){
+		let toCheck = 2 * round;
+		let i = toCheck * (matchNumber - this.matchesPlayed) - 1;
+		for(let last = i + toCheck - 1; i < last; i++){
+			if (this.players[i].lastWin = true){
+				this.sendNofification(this.players[i].connection,matchNumber);
+				if (!match.leftIndex)
+					match.leftIndex = i;
+				else{
+					match.rightIndex = i;
+					return;
+				}
+			}
+		}
+	}
 	getActivePlayers(){
 		return this.players.size;
 	}	
@@ -79,23 +119,17 @@ export class Room{
 	}
 
 	createMatches(){
-		let toCreate =  this.players.size / Math.pow(2,this.courentRound);
-		let matchId = this.players.size / toCreate;
+		let toCreate = this.players.size / Math.pow(2,this.courentRound);
+		let matchNum = this.matchesCreated + 1; 
 		for(i = 0; i < toCreate; i++){
-			const gameId = uuidv4();
-			games.set(gameId, {
-				gameState: initGame(),
-				clients: new Set(),
-				playersManager: new PlayersManager(),
-				intervalId: new Set(),
-				isRunning: false,
-				readyR: false,
-				readyL: false,
-				gameType: "",
-				needAuthentication: 1 //0 - no, 1 - optional 2 - required 
-			});		
+			let match = new Match();
+			this.getPlayersForMatch(match, matchNum);
+			this.matches.set(matchNum);
+			this.matchesCreated++;
+			matchNum++;
 		}
 	}
+
 }
 
 export class Player{
@@ -104,7 +138,9 @@ export class Player{
 	connection = null //for sending the notification
 	token = null; //to identify logged in players
 	nickname = null;
-	results = new Array(); //wins and looses, index is representing the round 
+	lastWin = true;
+	//results = new Array(); //wins and looses, index is representing the round ->
+	//->(For now useless)
 
 	constructor(connection,nickname,sessionId = null,token = null){
 		this.sessionId = sessionId;
@@ -115,7 +151,8 @@ export class Player{
 }
 
 export class Match{
-	gameId = uuidv4();
+	matchNumber = null;
+	gameId = uuidv4();//probably usless 
 	game = {
 				gameState: initGame(),
 				clients: new Set(),
@@ -124,13 +161,14 @@ export class Match{
 				isRunning: false,
 				readyR: false,
 				readyL: false,
-				gameType: "",
-				needAuthentication: 0 //0 - no, 1 - optional 2 - required 
+				gameType: "", //probably useless
+				needAuthentication: 1 //0 - no, 1 - optional 2 - required 
 	}
-	leftPlayer = null;
-	rightPlayer = null;
+	leftIndex = null;//token/sessionId to verify player
+	rightIndex = null;
 	winner = null; //null - match unfinished
 	leftScore = 0;
 	rightScore = 0;
+
 }
 
