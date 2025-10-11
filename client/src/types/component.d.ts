7664512a7f09ca5156a1330aa62dad498b30468a
mwiacek <maciejwiacek4@gmail.com>
2025-10-11 18:04:30 +0200

    Final project structure stabilization

--- client/src/types/component.d.ts
+++
@@ -1,3 +0,0 @@
-export type ComponentProps = {
-	classes?: string[];
-};
--- server/src/utils/response.js
+++
@@ -1,7 +0,0 @@
-export const sendResponse = (res, statusCode = 200, message, props = null) => {
-  res.status(statusCode).send({
-    success: statusCode < 400,
-    message,
-    ...props,
-  });
-};
