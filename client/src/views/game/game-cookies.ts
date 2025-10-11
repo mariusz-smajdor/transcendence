0f8cec92817e299f7e270573a32d9847760fc2cf
mwiacek <maciejwiacek4@gmail.com>
2025-10-11 18:04:30 +0200

    Clean main

--- client/src/views/game/game-cookies.ts
+++
@@ -1,6 +0,0 @@
-export function getCookie(name:string) :string | undefined{
-	return document.cookie
-        .split('; ')
-        .find(row => row.startsWith(name + '='))
-        ?.split('=')[1];
-}
