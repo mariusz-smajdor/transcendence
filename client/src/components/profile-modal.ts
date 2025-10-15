c6d5faabc7b0ed365f939f1bdb1f284231a172cd
Maciej Wiacek <maciejwiacek4@gmail.com>
2025-10-15 18:58:52 +0200

    Rename AI player to CPU

--- client/src/components/profile-modal.ts
+++ client/src/components/profile-modal.ts
@@ -324,14 +324,14 @@ export function ProfileModal(options: ProfileModalOptions) {
 		matches.forEach((match: MatchResult) => {
 			const row = TableRow({});
 			const opponentCell = TableCell({
-				content: match.gameType === 'CPU' ? 'nygus' : match.opponent.username,
+				content: match.gameType === 'CPU' ? 'CPU' : match.opponent.username,
 				classes: ['flex', 'items-center', 'gap-2'],
 			});
 			opponentCell.prepend(
 				Img({
 					src: getAvatarUrl(
 						match.opponent.avatar,
-						match.gameType === 'CPU' ? 'nygus' : match.opponent.username
+						match.gameType === 'CPU' ? 'CPU' : match.opponent.username
 					),
 					classes: ['w-8', 'h-8', 'rounded-full', 'border', 'border-accent'],
 					alt: 'Opponent avatar',
